07b754b2e96299848c526b3368c703722ef1a017
Sergei Sevriugin <sevriugin@gmail.com>
2019-01-29 15:11:36 +0300

    add ethereum

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenLoyalty } from '../ethereum/token-loyalty';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  token: TokenLoyalty;
+
+  constructor() {
+  }
 
   ngOnInit() {
+    console.log(`Landing loaded`);
+    this.token = new TokenLoyalty();
   }
 
 }
--- src/app/parameters.ts
+++ src/app/parameters.ts
@@ -3,6 +3,15 @@ export class RideParameters {
     maxtime: number = 1000 * 60 * 60 * 3;
 }
 
+export class Ethereum {
+    networks: {
+      rinkeby: 'https://rinkeby.infura.io/fyE6fpwJWFc6fBYSet1w',
+      local: 'http://localhost:8545'
+    };
+    mnemonic: 'first mix any adult deal sand brand about window will casual second';
+}
+
 export class Parameters {
     ride: RideParameters;
+    ethereum: Ethereum;
 }
